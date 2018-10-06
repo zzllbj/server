e71442fc5e7797e2e50848958798a044658d2f71
Monty <monty@mariadb.org>
2018-10-06 16:49:54 +0300

    Added backup handler calls
    Part of MDEV-5336 Implement LOCK FOR BACKUP
    
    Added support for backup calls in Aria to protect
    removal of redo logs.

--- sql/handler.cc
+++ sql/handler.cc
@@ -822,6 +822,40 @@ void ha_kill_query(THD* thd, enum thd_kill_levels level)
 }
 
 
+/*****************************************************************************
+  Backup functions
+******************************************************************************/
+
+static my_bool plugin_prepare_for_backup(THD *unused1, plugin_ref plugin,
+                                         void *not_used)
+{
+  handlerton *hton= plugin_hton(plugin);
+  if (hton->state == SHOW_OPTION_YES && hton->prepare_for_backup)
+    hton->prepare_for_backup();
+  return FALSE;
+}
+
+void ha_prepare_for_backup()
+{
+  plugin_foreach(NULL, plugin_prepare_for_backup, MYSQL_STORAGE_ENGINE_PLUGIN,
+                 NullS);
+}
+
+static my_bool plugin_end_backup(THD *unused1, plugin_ref plugin,
+                                 void *not_used)
+{
+  handlerton *hton= plugin_hton(plugin);
+  if (hton->state == SHOW_OPTION_YES && hton->end_backup)
+    hton->end_backup();
+  return FALSE;
+}
+
+void ha_end_backup()
+{
+  plugin_foreach(NULL, plugin_end_backup, MYSQL_STORAGE_ENGINE_PLUGIN, NullS);
+}
+
+
 /* ========================================================================
  ======================= TRANSACTIONS ===================================*/
 
