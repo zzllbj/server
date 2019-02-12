b8ea16164d6be59a1e5becfb81275fcc22206d9a
Vladislav Vaintroub <wlad@mariadb.com>
2019-02-12 09:12:02 +0100

    Fix link on older Ubuntu, fix ssl_crl test

--- include/sslopt-case.h
+++ include/sslopt-case.h
@@ -29,8 +29,11 @@
       One can disable SSL later by using --skip-ssl or --ssl=0
     */
       opt_use_ssl= 1;
+#ifdef HAVE_WOLFSSL
+      /* CRL does not work with WolfSSL */ 
       opt_ssl_crl= NULL;
       opt_ssl_crlpath= NULL;
+#endif
       break;
 #endif
 #endif /* SSLOPT_CASE_INCLUDED */
