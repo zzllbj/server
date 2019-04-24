db7b340e594680dec3f77e00d23af1f4ad0990bb
Vladislav Vaintroub <wlad@mariadb.com>
2019-04-24 11:15:08 +0200

    MDEV-18131 MariaDB does not verify IP addresses from subject alternative names
    Added a call to X509_check_ip_asc() in case server_hostname represents
    an IP address.

--- mysql-test/suite.pm
+++ mysql-test/suite.pm
@@ -72,9 +72,14 @@ sub skip_combinations {
     unless $::mysqld_variables{'version-ssl-library'} =~ /OpenSSL (\S+)/
        and $1 ge "1.0.1d";
 
-  $skip{'t/ssl_7937.combinations'} = [ 'x509v3' ]
-    unless $::mysqld_variables{'version-ssl-library'} =~ /OpenSSL (\S+)/
+  my $x509v3_ok =  $::mysqld_variables{'version-ssl-library'} =~ /OpenSSL (\S+)/
        and $1 ge "1.0.2";
+
+  $skip{'t/ssl_7937.combinations'} = [ 'x509v3' ]
+    unless $x509v3_ok;
+
+  $skip{'t/ssl_verify_ip.test'} = 'x509v3 support required'
+    unless $x509v3_ok;
 
   %skip;
 }
